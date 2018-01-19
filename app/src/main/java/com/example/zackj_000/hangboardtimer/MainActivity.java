25bbde0e4363c4455c8aa42c3d93ece4409434e6
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-19 14:26:46 -0800

    Java is working in this project. Commit before big modifications

--- app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zackj_000.hangboardtimer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button1 = findViewById(R.id.button1);
+
+        button1.setBackgroundColor(Color.BLUE);
+
+
+
     }
 }
