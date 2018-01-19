f472157f3122310a5f1bce024c3038778083a2c1
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-19 13:04:01 -0800

    First Commit

---
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zackj_000.hangboardtimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
