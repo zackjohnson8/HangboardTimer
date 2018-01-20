71cdd564c41d5a763ec0fb31381df9ce11a5be25
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-19 17:39:27 -0800

    Added buttons to the app. Issues came with layout modification to get exact locations no matter what size screen selected.

--- app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.zackj_000.hangboardtimer;
 
 import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewDebug;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +18,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = findViewById(R.id.button1);
+        Button buttonUpperLeft = findViewById(R.id.button1);
+        Button buttonUpperRight = findViewById(R.id.button2);
+        Button buttonBottomLeft = findViewById(R.id.button3);
+        Button buttonBottomRight = findViewById(R.id.button4);
+        View constraintLayout = findViewById(R.id.Layout_Constraint);
 
-        button1.setBackgroundColor(Color.BLUE);
+        buttonUpperLeft.setBackgroundColor(Color.BLUE);
+        buttonUpperRight.setBackgroundColor(Color.BLUE);
+        buttonBottomLeft.setBackgroundColor(Color.BLUE);
+        buttonBottomRight.setBackgroundColor(Color.BLUE);
 
+
+
+        // Get Screen size
+        Display displaySize = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        displaySize.getSize(size);
+
+        int maxX = size.x;
+        int maxY = size.y;
+
+        Log.d("DEBUG Width", String.valueOf(buttonUpperLeft.getWidth()));
+        Log.d("DEBUG Height", String.valueOf(buttonUpperLeft.getHeight()));
+
+        buttonUpperLeft.setX((maxX / 2));
+        buttonUpperLeft.setY((maxY / 2));
+
+        // Button color change for testing
+        //button1.setBackgroundColor(Color.BLUE);
 
 
     }
+
+
 }
