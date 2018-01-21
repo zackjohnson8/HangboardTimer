35b5b0fdf83b639dd1b66f66a46fb1540a8315c0
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-21 12:26:35 -0800

    Added: The listeners for each button. Implementing each buttons layout

--- app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/MainActivity.java
@@ -11,45 +11,56 @@ import android.view.ViewDebug;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonUpperLeft;
+    private Button buttonUpperRight;
+    private Button buttonBottomLeft;
+    private Button buttonBottomRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonUpperLeft = findViewById(R.id.button1);
-        Button buttonUpperRight = findViewById(R.id.button2);
-        Button buttonBottomLeft = findViewById(R.id.button3);
-        Button buttonBottomRight = findViewById(R.id.button4);
-        View constraintLayout = findViewById(R.id.Layout_Constraint);
+        buttonUpperLeft = findViewById(R.id.button1);
+        buttonUpperRight = findViewById(R.id.button2);
+        buttonBottomLeft = findViewById(R.id.button3);
+        buttonBottomRight = findViewById(R.id.button4);
 
-        buttonUpperLeft.setBackgroundColor(Color.BLUE);
-        buttonUpperRight.setBackgroundColor(Color.BLUE);
-        buttonBottomLeft.setBackgroundColor(Color.BLUE);
-        buttonBottomRight.setBackgroundColor(Color.BLUE);
+        //View constraintLayout = findViewById(R.id.Layout_Constraint);
+        //constraintLayout.setBackgroundColor(Color.parseColor("#000000"));
 
-
-
-        // Get Screen size
-        Display displaySize = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        displaySize.getSize(size);
-
-        int maxX = size.x;
-        int maxY = size.y;
-
-        Log.d("DEBUG Width", String.valueOf(buttonUpperLeft.getWidth()));
-        Log.d("DEBUG Height", String.valueOf(buttonUpperLeft.getHeight()));
-
-        buttonUpperLeft.setX((maxX / 2));
-        buttonUpperLeft.setY((maxY / 2));
-
-        // Button color change for testing
-        //button1.setBackgroundColor(Color.BLUE);
-
+        buttonUpperLeft.setOnClickListener(this);
+        buttonUpperRight.setOnClickListener(this);
+        buttonBottomLeft.setOnClickListener(this);
+        buttonBottomRight.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v)
+    {
 
+        switch(v.getId())
+        {
+            case R.id.button1:
+                buttonUpperLeft.setBackgroundColor(Color.BLUE);
+                setContentView(R.layout.activity_interval_training);
+                break;
+            case R.id.button2:
+                buttonUpperRight.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.button3:
+                buttonBottomLeft.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.button4:
+                buttonBottomRight.setBackgroundColor(Color.BLUE);
+                break;
+            default:
+                break;
+
+        }
+
+    }
 }
