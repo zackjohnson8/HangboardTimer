25e400affe79345fbfcd08bc1b3fb5b02ae5eb8e
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-24 12:52:25 -0800

    Added: Each of the java class and xml for each separate activity display

---
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/IntervalTraining.java
@@ -0,0 +1,20 @@
+package com.example.zackj_000.hangboardtimer;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+
+public class IntervalTraining extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interval_training);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/WeightTraining.java
@@ -0,0 +1,8 @@
+package com.example.zackj_000.hangboardtimer;
+
+/**
+ * Created by zackj_000 on 1/24/2018.
+ */
+
+public class WeightTraining {
+}
