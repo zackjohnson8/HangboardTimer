a038c01155fe27862f4291d47e244735b71c38d3
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-01-24 23:37:53 -0800

    ADDED: Basic timer now counts up. Still need to add functionality but has a basic working status.

--- app/src/main/java/com/example/zackj_000/hangboardtimer/BasicTimerActivity.java
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/BasicTimerActivity.java
@@ -4,15 +4,42 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.util.Log;
+import android.widget.TextView;
 
 public class BasicTimerActivity extends Activity implements View.OnClickListener
 {
 
     private Button buttonStart;
+    private TextView clockTimeText;
+    Handler customHandler = new Handler();
+
+    long startTime = 0L;
+    long timeInMilli = 0L;
+    long timeSwapBuff = 0L;
+    long updateTime = 0L;
+
+    Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilli = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff+timeInMilli;
+            int secs = (int)updateTime/1000;
+            int mins=secs/60;
+            secs%=60;
+            int milliseconds = (int)(updateTime%1000);
+
+
+            clockTimeText.setText(""+mins+":"+String.format("%02d",secs)+":"+String.format("%03d",milliseconds));
+            customHandler.postDelayed(this,0);
+        }
+    };
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,7 +47,7 @@ public class BasicTimerActivity extends Activity implements View.OnClickListener
         setContentView(R.layout.activity_basic_timer);
 
         buttonStart = findViewById(R.id.basicTimerButtonStart);
-        //buttonStart.setOnClickListener(this);
+        clockTimeText = findViewById(R.id.basicTimerCountTextView);
 
         buttonStart.setOnClickListener(this);
 
@@ -35,8 +62,6 @@ public class BasicTimerActivity extends Activity implements View.OnClickListener
         {
             case R.id.basicTimerButtonStart:
                 startTimer();
-                //buttonStart.setBackgroundColor(Color.BLUE);
-                //Intent to go back
                 //Intent MainActivity = new Intent(BasicTimerActivity.this, MainActivity.class);
                 //startActivity(MainActivity);
                 break;
@@ -53,12 +78,16 @@ public class BasicTimerActivity extends Activity implements View.OnClickListener
     private void startTimer()
     {
 
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread,0);
 
     }
 
     private void stopTimer()
     {
 
+        timeSwapBuff+=timeInMilli;
+        customHandler.removeCallbacks(updateTimerThread);
 
     }
 
