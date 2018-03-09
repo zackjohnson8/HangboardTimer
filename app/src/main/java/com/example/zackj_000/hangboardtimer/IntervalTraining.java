fbd64272fbf7a8c54de44328f1f7f5e628040d9f
Zachary L. Johnson <zackjohnson8@gmail.com>
2018-03-09 13:49:52 -0800

    Added: The scroll wheel for number choice in the Interval trainer screen

--- app/src/main/java/com/example/zackj_000/hangboardtimer/IntervalTraining.java
+++ app/src/main/java/com/example/zackj_000/hangboardtimer/IntervalTraining.java
@@ -1,20 +1,152 @@
 package com.example.zackj_000.hangboardtimer;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+//TODO(ZACK): Build out the directory buttons into a bottom layout.
+//AppCompatActivity
+public class IntervalTraining extends Activity implements View.OnClickListener
+{
+
+    private Button buttonStart;
+    private Button buttonStop;
+    private ImageButton buttonHome;
+    private TextView clockTimeText;
+    private NumberPicker npBreakTime;
+    Handler customHandler = new Handler();
 
 
-public class IntervalTraining extends Activity {
+    int extraMinutesTest = 0;
+    long startTime = 0L;
+    long timeInMilli = 0L;
+    long timeSwapBuff = 0L;
+    long updateTime = 0L;
+    boolean timerRunning = false;
+
+    Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilli = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff+timeInMilli;
+            int secs = (int)updateTime/1000;
+            int mins=(secs/60)+extraMinutesTest;
+            secs%=60;
+            int milliseconds = (int)(updateTime%1000);
+
+            clockTimeText.setText(""+mins+":"+String.format("%02d",secs)+"."+String.format("%03d",milliseconds));
+            customHandler.postDelayed(this,0);
+        }
+    };
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interval_training);
 
+        // StopWatch Handler
+        clockTimeText = findViewById(R.id.basicTimerCountTextView);
+        clockTimeText.setVisibility(View.GONE);
+
+        // NumberPicker Handler
+        NumberPicker npHangTime_p = findViewById(R.id.npHangTime);
+        npHangTime_p.setMinValue(1);
+        npHangTime_p.setMaxValue(60);
+
+        /*npHangTime_p.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                npHangTime_p.setValue(newVal);
+            }
+        });
+        */
+
+        // Button Handler
+        buttonStart = findViewById(R.id.basicTimerButtonStart);
+        buttonStop = findViewById(R.id.basicTimerButtonStop);
+        buttonHome = findViewById(R.id.buttonHome);
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonHome.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v)
+    {
+
+        switch(v.getId())
+        {
+
+            case R.id.buttonHome:
+                Intent BasicTimerTrainer = new Intent(IntervalTraining.this, MainActivity.class);
+                startActivity(BasicTimerTrainer);
+                break;
+
+            case R.id.basicTimerButtonStart:
+                if(!timerRunning) {
+                    timerRunning = !timerRunning;
+                    buttonStop.setText("STOP");
+                    startTimer();
+                }else
+                {
+                    clearTimer();
+                    startTimer();
+                }
+                //Intent MainActivity = new Intent(BasicTimerActivity.this, MainActivity.class);
+                //startActivity(MainActivity);
+                break;
+
+            case R.id.basicTimerButtonStop:
+                if(timerRunning) {
+                    timerRunning = !timerRunning;
+                    buttonStop.setText("RESET");
+                    stopTimer();
+                }else
+                {
+                    clearTimer();
+                    clockTimeText.setText("0:00.000");
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void startTimer()
+    {
+
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread,0);
+
+    }
+
+    private void stopTimer()
+    {
+
+        timeSwapBuff+=timeInMilli;
+        customHandler.removeCallbacks(updateTimerThread);
+
+    }
+
+    private void clearTimer()
+    {
+
+        startTime = 0L;
+        timeInMilli = 0L;
+        timeSwapBuff = 0L;
+        updateTime = 0L;
+
+    }
+
 
 }
